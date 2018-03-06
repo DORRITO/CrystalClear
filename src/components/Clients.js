271d8eac9d3006db2eddba6d434a1ae7467c1354
CHIp Wright <frisbeeprod@msn.com>
2018-03-06 01:55:09 -0700

    changed text to vw to stay in div

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -8,10 +8,10 @@ class Clients extends Component {
       <div className="Clients" >
         <Segment style={{marginTop: '1rem'}} className="Text-Services">
             <div className="Commercial" style={{marginLeft: '10%'}}>
-                <Header style={{margin: '2rem 0 25% 0', display: 'inline-block', fontFamily: 'Playball', fontSize: '300%'}} floated='centered'>Commercial</Header>
+                <Header style={{paddingLeft: '25%', display: 'inline-block', fontFamily: 'Playball', fontSize: '3.5vw'}} floated='centered'>Commercial</Header>
             </div>
             <div className="Residential" style={{marginLeft: '15%'}}>
-                <Header style={{margin: '2rem 0 25% 0', display: 'inline-block', fontFamily: 'Playball', fontSize: '300%', webkitTextStroke: '.1rem #000'}} floated='center'>Residential</Header>
+                <Header style={{paddingLeft: '25%', display: 'inline-block', fontFamily: 'Playball', fontSize: '3.5vw', webkitTextStroke: '.1rem #000'}} floated='center'>Residential</Header>
             </div>
         </Segment>
       </div>
